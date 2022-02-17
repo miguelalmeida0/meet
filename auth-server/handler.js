1aab91ed08f239643ade4dac81b43daf0e31ebe8
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-02-17 10:11:31 +0100

    updating main

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -68,7 +68,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*'
+          'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify(token),
       };
