5b3cde66fe4e9dc98e96fda3f59c95e64a8727e5
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-02-23 16:29:17 +0100

    fixing 4.5

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,19 +3,20 @@ import puppeteer from "puppeteer";
 describe("show/hide an event details", () => {
   let browser;
   let page;
-
   beforeAll(async () => {
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto("http://localhost:3000/");
-
     await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
   });
 
   test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .extra-details");
     expect(eventDetails).toBeNull();
-    browser.close();
   });
 });
