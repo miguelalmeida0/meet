1f7d1c0991298959beae0ddc99f86030c9a645e3
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-03-16 12:28:19 +0100

    Meet app :)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import NumberOfEvents from "./NumberOfEvents";
 import { OfflineAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import EventGenre from "./EventGenre";
 
 class App extends Component {
   state = {
@@ -75,6 +85,18 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined)
       return <div className="App" />;
@@ -92,6 +114,23 @@ class App extends Component {
             this.updateNumberOfEvents(number);
           }}
         />
+        <div className="data-vis-wrapper">
+          <EventGenre events={this.state.events} />
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis
+                allowDecimals={false}
+                type="number"
+                dataKey="number"
+                name="number of events"
+              />
+              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={events} numberOfEvents={numberOfEvents} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
